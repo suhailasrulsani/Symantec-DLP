849dc482004f3513255f338aef991718e9876751
Muhammad Suhail <suhail.asrul@gmail.com>
2021-03-31 13:20:00 +0800

    Update DLP.ps1

--- DLP.ps1
+++ DLP.ps1
@@ -4,8 +4,6 @@ Clear-Host
 #----------------------------------------------
 Get-PSSession | Remove-PSSession
 Remove-Variable * -ErrorAction SilentlyContinue; $Error.Clear();
-$Password = ConvertTo-SecureString "Welcome12" -AsPlainText -Force
-$Cred = New-Object System.Management.Automation.PSCredential ("globalnet\suhail_asrulsani-ops", $Password)
 $ScriptDir = Split-Path -Path $MyInvocation.MyCommand.Definition -Parent
 $Machinelist = @(get-content -Path "$ScriptDir\Machinelist.txt")
 $datetime = Get-Date -Format G
